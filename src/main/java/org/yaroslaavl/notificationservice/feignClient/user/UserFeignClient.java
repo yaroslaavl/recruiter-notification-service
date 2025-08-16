67fb4192207d99dcec62c3a21a2f1533c874f098
ylopatkin <ylopatkin@gmail.com>
2025-08-16 13:11:34 +0200

    16.08 - changed interface name

--- src/main/java/org/yaroslaavl/notificationservice/feignClient/user/UserFeignClient.java
+++ src/main/java/org/yaroslaavl/notificationservice/feignClient/user/UserFeignClient.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "user-service", path = "/api/v1")
-public interface UserAccountExists {
+public interface UserFeignClient {
 
     @GetMapping("/user/exists")
     Boolean existsAccount(@RequestParam("email") String email);
