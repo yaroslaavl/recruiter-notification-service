23ddc3da679c7b0e31db9dd6a66e00a6a518c1f3
ylopatkin <ylopatkin@gmail.com>
2025-09-18 22:40:05 +0200

    18.09 - refactored EmailVerificationController to EmailController and replaced EmailVerificationService with EmailService; enabled RabbitMQ integration in NotificationServiceApplication

--- src/main/java/org/yaroslaavl/notificationservice/controller/EmailController.java
+++ src/main/java/org/yaroslaavl/notificationservice/controller/EmailController.java
@@ -6,40 +6,40 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.yaroslaavl.notificationservice.dto.InitialRegistrationRequestDto;
-import org.yaroslaavl.notificationservice.service.EmailVerificationService;
+import org.yaroslaavl.notificationservice.service.EmailService;
 import org.yaroslaavl.notificationservice.validation.groups.CandidateAction;
 import org.yaroslaavl.notificationservice.validation.groups.RecruiterAction;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/mail")
-public class EmailVerificationController {
+public class EmailController {
 
-    private final EmailVerificationService emailVerificationService;
+    private final EmailService emailService;
 
     @PostMapping("/request-verification-candidate")
     @ResponseStatus(code = HttpStatus.NO_CONTENT)
     public void sendRequestVerificationCandidate(@RequestBody @Validated(CandidateAction.class)
                                                  InitialRegistrationRequestDto initialRegistrationRequestDto) {
-        emailVerificationService.requestVerification(initialRegistrationRequestDto);
+        emailService.requestVerification(initialRegistrationRequestDto);
     }
 
     @PostMapping("/request-verification-recruiter")
     @ResponseStatus(code = HttpStatus.NO_CONTENT)
     public void sendRequestVerificationRecruiter(@RequestBody @Validated(RecruiterAction.class)
                                                      InitialRegistrationRequestDto initialRegistrationRequestDto) {
-        emailVerificationService.requestVerification(initialRegistrationRequestDto);
+        emailService.requestVerification(initialRegistrationRequestDto);
     }
 
     @PostMapping("/verify-code")
     @ResponseStatus(code = HttpStatus.NO_CONTENT)
     public void verifyEmailCode(@RequestParam("verificationCode") String verificationCode,
                                 @RequestParam("email") String email) {
-        emailVerificationService.verifyCode(verificationCode, email);
+        emailService.verifyCode(verificationCode, email);
     }
 
     @GetMapping("/check")
     public ResponseEntity<String> checkEmailVerification(@RequestParam("email") String email) {
-        return ResponseEntity.ok(emailVerificationService.checkEmailVerification(email));
+        return ResponseEntity.ok(emailService.checkEmailVerification(email));
     }
 }
